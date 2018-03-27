d9dfe310756fb3dd5e33a039d6e9cefa0e6d87f1
michael hohl <mhohl@rocketmail.com>
2018-03-27 22:39:28 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,9 +1,9 @@
 class MusicLibraryController
 
-  attr_reader :path
-
   def initialize(path = "./db/mp3s")
-    @path = path
+    music_importer = MusicImporter.new(path)
+    music_importer.import
+  end
 
 
 
