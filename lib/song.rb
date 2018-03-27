f2dc3252b76b94a27e6a07510f5879486d4ecb76
michael hohl <mhohl@rocketmail.com>
2018-03-27 21:44:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
     self.find_by_name(name) || self.create(name)
   end
 
-  def self.new_by_filename(filename)
+  def self.new_from_filename(filename)
     array = filename.split(" - ")
     name = array[1]
     artist = array[0]
